6732e6004c2eda5ab82b25715f2171e5c97c04fa
thedarkknight0407 <149961789+thedarkknight0407@users.noreply.github.com>
2026-02-06 00:09:43 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const locked_hearts = document.querySelectorAll(".img-lock");
 const unlocked_hearts = document.querySelectorAll(".img-unlock");
 const date = new Date();
 const valentine = new Date("2026-02-14");
-const days_left = Math.ceil((valentine - date) / (1000 * 60 * 60 * 24));
+const days_left = Math.ceil((valentine - date-5.5 * 60 * 60 * 1000) / (1000 * 60 * 60 * 24));
 console.log(days_left);
 for (let i = 0; i < 10 - days_left; i++) {
   let element1 = locked_hearts[i];
