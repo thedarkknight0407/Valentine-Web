f5c59bc9af4d0f3e8e32e18e1ae9a37d43040033
Saksham Verma <vermasaksham0408@gmail.com>
2026-02-09 23:11:38 +0530

    Changed all data to be IST from GMT

--- js/feb-5.js
+++ js/feb-5.js
@@ -2,7 +2,13 @@ const body = document.getElementsByTagName("body");
 
 const today_date = new Date();
 const valentine = new Date("2026-02-14");
-if (10 - Math.round((valentine - today_date) / (3600 * 24000)) > 0) {
+if (
+  10 -
+    Math.round(
+      (valentine - today_date - 5.5 * 60 * 60 * 1000) / (3600 * 24000),
+    ) >
+  0
+) {
   body[0].classList.remove("d-none");
 }
 
--- js/feb-6.js
+++ js/feb-6.js
@@ -2,7 +2,13 @@ const body = document.getElementsByTagName("body");
 
 const today_date = new Date();
 const valentine = new Date("2026-02-14");
-if (10 - Math.ceil((valentine - today_date-5.5 * 60 * 60 * 1000) / (3600 * 24000)) > 1) {
+if (
+  10 -
+    Math.ceil(
+      (valentine - today_date - 5.5 * 60 * 60 * 1000) / (3600 * 24000),
+    ) >
+  1
+) {
   body[0].classList.remove("d-none");
 } else {
   window.location.href = "shanpatti.html";
